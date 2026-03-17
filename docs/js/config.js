8bd389541a79e188d33f7b6a97a2ffe9fc5b48e3
jiangsir <jiangzero@gmail.com>
2026-03-17 10:26:22 +0800

    更新 GAS_URL 至最新的 Google Apps Script 網址

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,2 +1,3 @@
-const GAS_URL = 'https://script.google.com/a/macros/stu.nknush.kh.edu.tw/s/AKfycbxXZPu3KUjy1NhsgO4drQakKCJ2wsAP-v2lQzOzZmUV2lcE_vJ8HxY3J76jEF81o4p0/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzDYO4jY30u3UWZiFtFYH1lA7Pytw3y5LsZQM2Cx4sJlkUcRpUzajXn-e9UI_lDX3oFUQ/exec';
+// https://script.google.com/macros/s/AKfycbzDYO4jY30u3UWZiFtFYH1lA7Pytw3y5LsZQM2Cx4sJlkUcRpUzajXn-e9UI_lDX3oFUQ/exec
 // https://script.google.com/macros/s/AKfycbxXZPu3KUjy1NhsgO4drQakKCJ2wsAP-v2lQzOzZmUV2lcE_vJ8HxY3J76jEF81o4p0/exec
